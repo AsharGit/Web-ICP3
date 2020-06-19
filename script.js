5d78950512e369b33f83cbaf0b43cf25ea4862cf
AsharGit <32973667+AsharGit@users.noreply.github.com>
2020-06-19 14:00:25 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,55 @@
-var compHand = Math.floor(Math.random() * 3)
+function choice(input) {
+    switch (input) {
+        case 0:
+            return "Rock"
+        case 1:
+            return "Paper"
+        case 2:
+            return "Scissors"
+        default:
+            break;
+    }
+}
 
-function game()
-{
-    document.write("The computer chose: ")
-    document.write(comphand)
+function  resultMessage(msg) {
+    var message = document.getElementById("message");
+    var info = document.createElement("div");
+    info.textContent = msg;
+    message.appendChild(info);
+}
+
+function result(playHand, compHand) {
+    if (playHand === compHand) {
+        resultMessage("It's a tie!")
+    } else if (playHand === "Rock") {
+        if (compHand === "Paper") {
+            resultMessage("You lose!")
+        } else {
+            resultMessage("You Win!")
+        }
+    } else if (playHand === "Paper") {
+        if (compHand === "Scissors") {
+            resultMessage("You lose!")
+        } else {
+            resultMessage("You Win!")
+        }
+    } else if (playHand === "Scissors") {
+        if (compHand === "Rock") {
+            resultMessage("You lose!")
+        } else {
+            resultMessage("You Win!")
+        }
+    }
+}
+
+
+function game(input) {
+    var comp = Math.floor(Math.random() * 3)
+    var compHand = choice(comp)
+    var playHand = input
+
+    resultMessage("You chose: " + playHand)
+    resultMessage("The computer chose: " + compHand)
+    result(playHand, compHand)
+    console.clear()
 }
